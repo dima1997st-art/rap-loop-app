191c8a9ffc10c12dd4082b1a56bb9bcd0bf9513c
dima1997st-art <dima1997st@gmail.com>
2026-06-19 19:56:04 +0300

    remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-      crypto: false,
-    };
-
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
